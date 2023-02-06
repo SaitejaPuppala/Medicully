0d5b8af0740706eb5530d5cb0f1921de4bdb4f73
Saiteja Puppala <37582288+SaitejaPuppala@users.noreply.github.com>
2023-02-06 02:02:10 -0800

    Update controllers.js

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -32,7 +32,7 @@ var transporter = nodemailer.createTransport(smtpTransport({
   }
 }));
 
-mongoose.connect('mongodb+srv://medicully:8mW2IxjmoQhDI5aQ@medicully.cbaamnc.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://medicully:8mW2IxjmoQhDI5aQ@medicully.cbaamnc.mongodb.net/medicully', {dbName: "medicully"}, { useNewUrlParser: true });
 var date_crte = function(){
   var currentTime = new Date();
   var currentOffset = currentTime.getTimezoneOffset();
